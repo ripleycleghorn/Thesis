19a62ecabbe419078032e04907cc121552a092b1
Ripley Cleghorn <ripley.cleghorn@gmail.com>
2021-04-06 10:45:44 -0400

    add project code

---
+++ project-code/src/components/XAxis.vue
@@ -0,0 +1,28 @@
+<template>
+  <g 
+    class="axis x-axis"
+    :transform="`translate(30, ${height})`"
+  >
+  </g>
+</template>
+
+<script>
+import { select, axisBottom } from 'd3'
+
+export default {
+  name: 'XAxis',
+  props: {
+    xScale: Function,
+    height: Number
+  },
+  updated() {
+    select('.x-axis')
+      .call(axisBottom(this.xScale).ticks(10))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
