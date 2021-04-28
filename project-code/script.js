a9158dd1a44f74f922c11abe2b57b8e3b5770fac
Ripley Cleghorn <ripley.cleghorn@gmail.com>
2021-04-28 17:15:55 -0400

    added tooltip (not working)

--- project-code/script.js
+++ project-code/script.js
@@ -130,6 +130,29 @@ function handleData(data) {
       d3.select('.graph-center')
         .attr('class', 'graph-center hidden')
     }
+    if (pageData.hover) {
+      var div = d3.select("#geoengineering")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+        // setup svg & add group
+        let hover = d3.select('#geoengineering')
+            .on("mouseover", function(event) {
+                div.transition()
+                  .duration(200)
+                  .style("opacity", .9);
+                div.html("Geoengineering: To make a large-scale effort to modify the earth or its environment, especially to counteract global warming")
+                  .style("left", (900) + "px")
+                  .style("top", (150) + "px")
+                  .style("position", "absolute");
+                })
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+                });
+    }
   }
 
   function drawGraph(x, y, graphData) {
@@ -199,6 +222,11 @@ function handleData(data) {
     d3.select('#y-axis')
       .transition()
       .call(yAxis)
+  }
+
+  //show tooltip
+  function tooltip() {
+    
   }
 
   //underline current section header
